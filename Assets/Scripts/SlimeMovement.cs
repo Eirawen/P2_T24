3f2ec045ccb6f8a0825fc0757c8d5ef2a4efd0c9
Eirawen <kmessai@stanford.edu>
2024-05-10 17:54:12 -0700

    BLue Green SUpremacy

--- Assets/Scripts/SlimeMovement.cs
+++ Assets/Scripts/SlimeMovement.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 public class SlimeMovement : MonoBehaviour {
-    public float speed = 5.0f; // how fast the player moves by default
+    public float speed = 10.0f; // how fast the player moves by default
     public float jumpForce = 10.0f; // how high the player jumps
     public float movementSmooth = 0.1f; // how smooth the movement is, play around with this value
     public float groundCheckDistance = 0.1f; // how far down to check for ground
@@ -9,7 +9,8 @@ public class SlimeMovement : MonoBehaviour {
     public LayerMask groundLayer; //defines the ground layer - useful for checking collision with the ground
     private Rigidbody2D rb; // rigidbody2d is the unity physics component that allows the player to move
     private Vector2 velocitySmooth; // smooths out the movement
-    public bool isGrounded; // checks if the player is on the ground
+    public bool isPlayerGrounded; // checks if the player is on the ground
+    public bool isPlayerMoving; // checks if the player is moving
 
     private float moveInput; // stores the player's input for movement
 
@@ -25,15 +26,16 @@ public class SlimeMovement : MonoBehaviour {
         moveInput = Input.GetAxis("Horizontal");
         MovePlayer(); 
         if (Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("Jump");
             Jump();
         }
     }
 
     void MovePlayer() { // defines player movement
         Vector2 targetVelocity = new Vector2(moveInput * speed, rb.velocity.y);
-        isGrounded = IsGrounded();
+        isPlayerGrounded = isGrounded();
 
-        if (isGrounded) {
+        if (isPlayerGrounded) {
             rb.velocity = Vector2.SmoothDamp(rb.velocity, targetVelocity, ref velocitySmooth, movementSmooth);
         } else {
             targetVelocity *= airControl;
@@ -42,15 +44,23 @@ public class SlimeMovement : MonoBehaviour {
     }
 
     void Jump() {
-        if (isGrounded) {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
+        isPlayerGrounded = isGrounded();
+        //if (isGrounded) {
+           // rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        //}
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
-    bool IsGrounded() {
+    bool isGrounded() {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
         return hit.collider != null;
     }
+
+
+    bool isMoving() {
+        return rb.velocity.x != 0;
+    }
+
 
 
 
