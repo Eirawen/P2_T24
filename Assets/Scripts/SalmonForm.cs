d29e1cbab73538735acb473e6e298696d68fb6bc
Eirawen <kmessai@stanford.edu>
2024-05-16 11:39:19 -0700

    Formswap finally fixed, Salmon can now only jump in water, Salmon gravity modifier added.

--- Assets/Scripts/SalmonForm.cs
+++ Assets/Scripts/SalmonForm.cs
@@ -3,16 +3,23 @@ using UnityEngine;
 public class SalmonForm : PlayerController {
 
     private bool inWater = false;
-
     private int waterLayer; 
 
-    public float salmonScale = 0.4f;
+    public float salmonJumpScale = 0.4f;
+
+
 
     public Sprite salmonSprite;
 
     public PlayerController pc; 
 
-    // In order for these methods to work, we must have a collider object set to "is trigger" rather
+    public SpriteRenderer spriteRenderer;
+
+
+    public float salmonGravityScale = 0.3f; 
+    public float normalGravityScale = 1.0f;
+
+    // In order for collision checking to work, we must have a collider object set to "is trigger" on water objects rather
     // than other types of collision. This tells Unity we're using it to trigger things rather than
     // to block movement. 
 
@@ -20,9 +27,17 @@ public class SalmonForm : PlayerController {
         // set the ground layer to the water layer
         waterLayer = LayerMask.NameToLayer("Water");
         // set the sprite to the salmon sprite
-        pc = gameObject.GetComponent<PlayerController>();
-        gameObject.GetComponent<SpriteRenderer>().sprite = pc.Sprites[1];
     }
+
+    protected override void Awake() {
+            base.Awake();
+            pc = gameObject.GetComponent<PlayerController>();
+            spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            salmonSprite = pc.Sprites[1];
+        }
+
+
+
     private void OnTriggerEnter2D(Collider2D water) {
         // if we are in water, change the flag. 
         if (water.gameObject.layer == waterLayer) {
@@ -30,10 +45,7 @@ public class SalmonForm : PlayerController {
         }
     }
 
-    protected override void Awake() {
-        base.Awake();
-        
-    }
+    
 
     // Unity's method called when the Collider other exits the trigger
     private void OnTriggerExit2D(Collider2D water) {
@@ -45,12 +57,6 @@ public class SalmonForm : PlayerController {
 
 
     protected override void MovePlayer() {
-        // TODO : override salmon movement
-
-        //need to further develop.
-
-        // also these curly braces are messed up i was messing around in emacs ill fix it later. 
-
         if (inWater) {
             base.MovePlayer();
         } else {
@@ -62,14 +68,15 @@ public class SalmonForm : PlayerController {
         moveInput = Input.GetAxis("Horizontal");
         MovePlayer();
         if (Input.GetKeyDown(KeyCode.Space)) {
-            Debug.Log("Jump");
-            Jump();
+            if (inWater) {
+                Jump();
+            }
         }
     }
 
 
     protected override void Jump() {
-        rb.AddForce(Vector2.up * jumpForce * salmonScale, ForceMode2D.Impulse);
+        rb.AddForce(Vector2.up * jumpForce * salmonJumpScale, ForceMode2D.Impulse);
     }
 
 
@@ -78,7 +85,16 @@ public class SalmonForm : PlayerController {
     }
 
     public override void SwitchSprite() {
-        gameObject.GetComponent<SpriteRenderer>().sprite = pc.Sprites[1];
+        spriteRenderer.sprite = salmonSprite;
+    }
+
+    void OnEnable() {
+        Debug.Log("Salmon form enabled");
+        rb.gravityScale = salmonGravityScale;
+    }
+
+    void OnDisable() {
+        rb.gravityScale = normalGravityScale;
     }
 }
     
--- Assets/Scripts/SlimeForm.cs
+++ Assets/Scripts/SlimeForm.cs
@@ -4,27 +4,33 @@ public class SlimeForm : PlayerController {
     private bool onWall = false;
     public Sprite slimeSprite;
 
+    public SpriteRenderer spriteRenderer;
+
     public PlayerController pc;
 
+
     void Start() {
-        pc = gameObject.GetComponent<PlayerController>();
-        gameObject.GetComponent<SpriteRenderer>().sprite = pc.Sprites[0];
+        
     }
 
     protected override void Awake() {
         base.Awake();
+        pc = gameObject.GetComponent<PlayerController>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        slimeSprite = pc.Sprites[0];
     }
     protected override void MovePlayer() {
-        // TODO : override salmon movement
 
-        //need to further develop.
-
-        // also these curly braces are messed up i was messing around in emacs ill fix it later. 
 
         base.MovePlayer();
     }
 
     public override void SwitchSprite() {
-        gameObject.GetComponent<SpriteRenderer>().sprite = pc.Sprites[0];
+        spriteRenderer.sprite = slimeSprite;
+    }
+
+    void OnEnable() {
+        Debug.Log("Slime form enabled");
+        
     }
 }
