f58f3e2e1c0b221ce974a5aea229bf5b4fd6da42
alexh555 <alexh555@stanford.edu>
2024-05-23 15:30:39 -0700

    water scene added sprites

---
+++ Assets/Scripts/RaiseWater.cs
@@ -0,0 +1,17 @@
+// // when the slimeform hits a button with this script, the water will raise up.
+
+
+// private void Raise() {
+        
+//         // boxCollider.size = new Vector2(originalSlimeSize.x * compressWidthScale, originalSlimeSize.y * compressHeightScale);
+//         // spriteRenderer.sprite = compressedSprite;
+//     }
+
+//     private void Lower() {
+//         // bool compressable = canLower();
+//         // if (compressable) {
+//         // boxCollider.size = originalSlimeSize;
+//         // SwitchSprite();
+//         // }
+
+//     }
