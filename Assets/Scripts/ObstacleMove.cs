2d6a55c3a56a445d2c8eff5740419ad69a120db7
alexh555 <alexh555@stanford.edu>
2024-06-04 14:35:39 -0700

    player obstacle bug fix

--- Assets/Scripts/ObstacleMove.cs
+++ Assets/Scripts/ObstacleMove.cs
@@ -23,6 +23,9 @@ public class ObstacleMove : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        if (collision.gameObject.tag != "Player"){
+            Destroy(gameObject);
+        }
+       
     }
 }
